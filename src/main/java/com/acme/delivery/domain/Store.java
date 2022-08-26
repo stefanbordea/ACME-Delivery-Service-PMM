2c292dae598d49ef66f6e3bf6b177c5148b8c4f8
Stefan Bordea <stefan@stefanbordea.com>
2022-08-26 15:50:42 +0300

    Create JPA entities for Store, Product and Card
    In the Store entity, Set<Product> is a many to many relationship because many products can be served in different stores e.g most pizzerias have pepperoni pizza

--- src/main/java/com/acme/delivery/domain/Store.java
+++ src/main/java/com/acme/delivery/domain/Store.java
@@ -1,15 +1,48 @@
 package com.acme.delivery.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 import java.time.DayOfWeek;
 import java.util.Set;
 
+@Getter
+@Setter
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "STORES")
+@SequenceGenerator(name = "idGenerator", sequenceName = "STORES_SEQ", allocationSize = 1)
 public class Store extends BaseModel {
 
-    private String name;
-    private String phoneNumber;
-    private String email;
-    private Set<DayOfWeek> openDays;
-    private Set<StoreCategory> categories;
-    private Address address;
-    private Set<Product> productsMenu;
+	@Column(length = 50, nullable = false, unique = true)
+	private String name;
+	@Column(length = 15, nullable = false)
+	private String phoneNumber;
+	@Column(length = 50, nullable = false)
+	private String email;
+	@Enumerated(EnumType.STRING)
+	private Set<DayOfWeek> openDays;
+	@Enumerated(EnumType.STRING)
+	private Set<StoreCategory> categories;
+	@Column(length = 50, nullable = false, unique = true)
+	private Address address;
+	@ManyToMany
+	@JoinTable(name = "store_product", joinColumns = {@JoinColumn(name = "store_id")}, inverseJoinColumns = {@JoinColumn(name = "product_id")})
+	private Set<Product> productsMenu;
 }
