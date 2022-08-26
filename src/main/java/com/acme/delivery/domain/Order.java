f858e68220306d6ad12e5e465a5358f659dc5c1b
a.chamalidis <antonisswim@hotmail.com>
2022-08-26 16:00:37 +0300

    Annotate domain class Order and OrderItem

--- src/main/java/com/acme/delivery/domain/Order.java
+++ src/main/java/com/acme/delivery/domain/Order.java
@@ -1,16 +1,38 @@
 package com.acme.delivery.domain;
 
+import lombok.Data;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.*;
 import java.math.BigDecimal;
 import java.util.Date;
 import java.util.Set;
 
-public class Order extends BaseModel {
-
+@Data
+@SuperBuilder
+@ToString(callSuper = true)
+@Entity
+@Table(name = "ORDER", indexes = {@Index(name = "ORDER_IDX_01", columnList = "serial")})
+@SequenceGenerator(name = "idGenerator", sequenceName = "ORDER_SEQ", initialValue = 1, allocationSize = 1)
+public class Order extends BaseModel
+{
+  @ManyToOne
+  @JoinColumn(name = "customer_id")
     private Account customer;
+	@ManyToOne
+	@JoinColumn(name = "deliveryAddress_id")
     private Address deliveryAddress;
+	@Enumerated(EnumType.STRING)
+	@Column(length = 20, nullable = false)
     private PaymentMethod paymentMethod;
+	@Column(length = 50, nullable = false)
     private String serial;
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(nullable = false)
     private Date date;
+	@Column(precision = 10, scale = 2, nullable = false)
     private BigDecimal totalPrice;
+	@OneToMany(mappedBy = "order",fetch = FetchType.EAGER,cascade = CascadeType.ALL)
     private Set<OrderItem> orderItems;
 }
--- src/main/java/com/acme/delivery/domain/OrderItem.java
+++ src/main/java/com/acme/delivery/domain/OrderItem.java
@@ -1,12 +1,32 @@
 package com.acme.delivery.domain;
 
-import java.math.BigDecimal;
+import lombok.Data;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import java.math.BigDecimal;
+@Data
+@SuperBuilder
+@ToString(callSuper = true)
+@Entity
+@Table(name = "ORDER_ITEM")
+@SequenceGenerator(name = "idGenerator", sequenceName = "ORDER_ITEM_SEQ", initialValue = 1, allocationSize = 1)
 public class OrderItem extends BaseModel {
 
     private Integer quantity;
+	@Column(precision = 10, scale = 2, nullable = false)
     private BigDecimal price;
+	@ManyToOne
+	@JoinColumn(name = "deliveryAddress_id")
     private Product product;
+	@ManyToOne
+	@JoinColumn(name = "order_id")
     private Order order;
 
 }
