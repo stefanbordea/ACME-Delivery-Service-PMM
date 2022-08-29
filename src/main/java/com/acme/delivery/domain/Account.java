3486dd1d4a0b28f82d6eac193018b6d206ba63b0
v.panou <vistoriapan98@gmail.com>
2022-08-29 10:37:28 +0300

    Implement Account class and AccountRepository interface.

--- src/main/java/com/acme/delivery/domain/Account.java
+++ src/main/java/com/acme/delivery/domain/Account.java
@@ -1,13 +1,36 @@
 package com.acme.delivery.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
 import java.util.Set;
 
+@Getter
+@Setter
+@ToString(callSuper = true)
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "ACCOUNT", indexes = {@Index(name = "ACCOUNT_IDX_01", columnList = "email")})
+@SequenceGenerator(name = "idGenerator", sequenceName = "ACCOUNT_SEQ", allocationSize = 1)
 public class Account extends BaseModel {
 
+	@Column(length = 50, nullable = false, unique = true)
     private String email;
+	@Column(length = 30, nullable = false)
     private String password;
+	@Column(length = 30, nullable = false, unique = true)
     private String phoneNumber;
+	@OneToMany(mappedBy = "account", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     private Set<Address> addresses;
+	@OneToMany(mappedBy = "account", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     private Set<Card> savedCards;
+	@OneToMany(mappedBy = "account", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     private Set<Order> pastOrders;
 }
---
+++ src/main/java/com/acme/delivery/repository/AccountRepository.java
@@ -0,0 +1,14 @@
+package com.acme.delivery.repository;
+
+import com.acme.delivery.domain.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, Long> {
+
+	Account findByEmail(String email);
+
+	Account findByPhoneNumber(String phoneNumber);
+
+}
