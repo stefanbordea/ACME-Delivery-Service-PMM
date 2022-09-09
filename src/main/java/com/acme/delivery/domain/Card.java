1b6b71d616abf564e97505cfc195baeba212a35a
v.panou <vistoriapan98@gmail.com>
2022-09-09 18:44:18 +0300

    Added validation annotations to Product and Card classes

--- src/main/java/com/acme/delivery/domain/Card.java
+++ src/main/java/com/acme/delivery/domain/Card.java
@@ -11,6 +11,8 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 @Getter
@@ -25,11 +27,16 @@ import java.time.LocalDate;
 public class Card extends BaseModel {
 
 	@Column(length = 16, nullable = false)
+	@NotNull
 	private Long cardNumber;
 	@Column(length = 30, nullable = false)
+	@NotNull
+	@NotEmpty
 	private String cardName;
 	@Column(length = 5, nullable = false)
+	@NotNull
 	private LocalDate cardExpirationDate;
 	@Column(length = 3, nullable = false)
+	@NotNull
 	private Integer cardSecurityCode;
 }
--- src/main/java/com/acme/delivery/domain/Product.java
+++ src/main/java/com/acme/delivery/domain/Product.java
@@ -14,6 +14,8 @@ import javax.persistence.Enumerated;
 import javax.persistence.Index;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
 @Getter
@@ -28,12 +30,17 @@ import java.math.BigDecimal;
 public class Product extends BaseModel {
 
 	@Column(length = 30, nullable = false, unique = true)
+	@NotNull
+	@NotEmpty
 	private String serial;
 	@Column(length = 50, nullable = false)
+	@NotEmpty
 	private String name;
 	@Column(length = 100)
+	@NotEmpty
 	private String description;
 	@Column(precision = 10, scale = 2, nullable = false)
+	@NotNull
 	private BigDecimal price;
 	@Enumerated(EnumType.STRING)
 	private ProductCategory categories;
