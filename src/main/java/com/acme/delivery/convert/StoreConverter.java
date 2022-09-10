79d66194ac773bbcfac33c89532a227b1f4d6923
Stefan Bordea <stefan@stefanbordea.com>
2022-09-10 15:53:49 +0300

    Create StoreDTO and its converters

---
+++ src/main/java/com/acme/delivery/convert/StoreConverter.java
@@ -0,0 +1,30 @@
+package com.acme.delivery.convert;
+
+
+import com.acme.delivery.domain.Store;
+import com.acme.delivery.transfer.StoreDTO;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Component
+public class StoreConverter {
+
+	private final ModelMapper mapper;
+
+	public Store dtoToEntity(StoreDTO storeDto) {
+		return mapper.map(storeDto, Store.class);
+	}
+
+	public StoreDTO entityToDto(Store store) {
+		return mapper.map(store, StoreDTO.class);
+	}
+
+	public List<StoreDTO> entityToDto(List<Store> stores) {
+		return stores.stream().map(this::entityToDto).toList();
+	}
+
+}
---
+++ src/main/java/com/acme/delivery/transfer/StoreDTO.java
@@ -0,0 +1,20 @@
+package com.acme.delivery.transfer;
+
+import com.acme.delivery.domain.Address;
+import com.acme.delivery.domain.Product;
+import com.acme.delivery.domain.StoreCategory;
+import lombok.Data;
+
+import java.util.Set;
+
+@Data
+public class StoreDTO {
+
+	private String name;
+	private String phoneNumber;
+	private String email;
+	private String openDays;
+	private StoreCategory category;
+	private Address address;
+	private Set<Product> productsMenu;
+}
