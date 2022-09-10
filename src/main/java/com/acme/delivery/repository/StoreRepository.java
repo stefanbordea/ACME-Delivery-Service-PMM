eef6fc915d710b6b9038e59f72676e23d5501580
Stefan Bordea <stefan@stefanbordea.com>
2022-09-10 15:50:57 +0300

    Create findStoreById method Also change findStoresByCategories to singular, so that we look up a store only by one category

--- src/main/java/com/acme/delivery/repository/StoreRepository.java
+++ src/main/java/com/acme/delivery/repository/StoreRepository.java
@@ -5,10 +5,15 @@ import com.acme.delivery.domain.StoreCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface StoreRepository extends JpaRepository<Store, Long> {
 
 	Store findByName(String name);
 
-	Store findStoresByCategories(StoreCategory[] categories);
+	List<Store> findStoresByCategory(StoreCategory category);
+
+	Store findStoreById(Long id);
+
 }
